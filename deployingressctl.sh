87d3b1c6d31217027361cece5894b7df5662aea9
Cedric Husianycia <cehusian@microsoft.com>
2020-10-08 10:30:51 +0200

    ingress first try

---
+++ deployingressctl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rg=teamResources
+aksname=team6akscluster
+
+#az aks enable-addons --resource-group $rg --name $aksname --addons http_application_routing
+#az aks show --resource-group $rg --name $aksname --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName -o table
+#c17d6ed3cc5545ffb648.northeurope.aksapp.io
